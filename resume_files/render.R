3505b2ad5b7fd15485742c8e51fa309a93f6d38e
Lisa Oshita <lisaoshita@Lisas-MacBook-Pro.local>
2020-02-19 19:51:59 -0800

    remove intro

--- resume_files/render.R
+++ resume_files/render.R
@@ -1,2 +1,2 @@
 # For PDF (with black italic links)
-rmarkdown::render("index.Rmd", output_file = "cv_for_pdf.html", params = list(PDF_EXPORT = TRUE))
+rmarkdown::render("~/Projects/resume/resume_files/index.Rmd", output_file = "cv_for_pdf.html", params = list(PDF_EXPORT = TRUE))
