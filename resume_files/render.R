50b772c5ebf9f3fee33e5ecacbdb21287c64974f
Lisa Oshita <lisaoshita@Lisas-MacBook-Pro.local>
2020-02-21 09:37:27 -0800

    add scott's suggestions

--- resume_files/render.R
+++ resume_files/render.R
@@ -1,2 +1,3 @@
 # For PDF (with black italic links)
-rmarkdown::render("~/Projects/resume/resume_files/index.Rmd", output_file = "cv_for_pdf.html", params = list(PDF_EXPORT = TRUE))
+rmarkdown::render("resume_files/index.Rmd", output_file = "cv_for_pdf.html", params = list(PDF_EXPORT = TRUE))
+
